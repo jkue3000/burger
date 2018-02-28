394aaa93e355c1defaee28ca86ddedaa6c11540b
unknown <jkue3000@gmail.com>
2018-02-28 18:22:26 -0500

    Devour button not functioning

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Vegetarian Sprouts Burger');
+INSERT INTO burgers (burger_name) VALUES ('BreakDown Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Salchica Burger');
+INSERT INTO burgers (burger_name) VALUES ('John Mcain Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fried Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('DIY Burger');
